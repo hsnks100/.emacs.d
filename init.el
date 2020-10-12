5e77665953f3107fbe6d88e4724b5714062f5bb1
디알_한경수 <dire@afreecatv.com>
2020-10-12 16:02:40 +0900

    company

--- init.el
+++ init.el
@@ -9,40 +9,10 @@
   (package-install 'use-package))
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "1bdc49116a77e52aaea740cd8e54b93e0bae6c0895dcc36d5c8d1a493e89c78d" "ea066684e9ace1e618719fab683b24a0fbcd3de82692190b1fe54e6b1b2a29bc" default))
- '(global-linum-mode t)
- '(grep-files-aliases
-   '(("all" . "* .[!.]* ..?*")
-     ("el" . "*.el")
-     ("ch" . "*.[ch]")
-     ("c" . "*.c")
-     ("cc" . "*.h *.cc *.cxx *.cpp *.C *.CC *.c++")
-     ("cchh" . "*.cc *.[ch]xx *.[ch]pp *.[CHh] *.CC *.HH *.[ch]++")
-     ("hh" . "*.hxx *.hpp *.[Hh] *.HH *.h++")
-     ("h" . "*.h")
-     ("l" . "[Cc]hange[Ll]og*")
-     ("m" . "[Mm]akefile*")
-     ("tex" . "*.tex")
-     ("texi" . "*.texi")
-     ("asm" . "*.[sS]")))
- '(helm-completion-style 'emacs)
- '(ivy-mode t)
- '(package-selected-packages
-   '(amx counsel smooth-scrolling try ivy-smex helm-smex smex-ivy csharp-mode csharp org-trello org-ioslide org-page eyebrowse treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs neotree smex helm airline-themes powerline atom-one-dark-theme highlight-numbers use-package ivy evil))
- '(treemacs-follow-mode t)
- '(ws-butler-global-mode t))
+(org-babel-load-file (expand-file-name "~/.emacs.d/setting.org"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-
-(org-babel-load-file (expand-file-name "~/.emacs.d/setting.org"))
